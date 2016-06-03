12a2bc4f00689d6551d20f3fd68b228b84e93f1b
L. Guo <iml885203@gmail.com>
2016-06-03 18:17:32 +0800

    更新 重複登入問題 update gitignore

--- php-member/index.php
+++ php-member/index.php
@@ -1,15 +1,25 @@
-﻿<html>
- <head>
-  <title>首頁</title>
- </head>
- <body>
- <!-- 設定網頁編碼為UTF-8 -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<form name="form" method="post" action="connect.php">
-帳號：<input type="text" name="id" /> <br>
-密碼：<input type="password" name="pw" /> <br>
-<input type="submit" name="button" value="登入" />&nbsp;&nbsp;
-<a href="register.php">申請帳號</a>
-</form>
- </body>
+<html>
+  <head>
+    <title>首頁</title>
+  </head>
+  <body>
+    <?php session_start(); ?>
+    <!-- 設定網頁編碼為UTF-8 -->
+    <?php
+      if (isset($_SESSION['username'])) {
+          echo '<meta http-equiv=REFRESH CONTENT=0;url=member.php>';
+      } else {
+    ?>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <form name="form" method="post" action="connect.php">
+      帳號：<input type="text" name="id" /> <br>
+      密碼：<input type="password" name="pw" /> <br>
+      <input type="submit" name="button" value="登入" />&nbsp;&nbsp;
+      <a href="register.php">申請帳號</a>
+    </form>
+    <?php } ?>
+
+
+  </body>
 </html>
